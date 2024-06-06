ce25ac8fff0512d5d7c14d58aa885ffb12d12f86
Christian Santos <it.christiansantos@gmail.com>
2024-06-06 11:59:09 +0800

    Template File

---
+++ template-to-use/Read-me-before-copy.php
@@ -0,0 +1,7 @@
+Hello, sa mga gusto kumuha ng template dito.
+anjan ang template , and css file link
+Copy then paste lang and if wala sa css ng template na yon wag galawin,
+ gawin mo ilagay nalang sa seperate css file para sa page na yon.
+ Like add ka ng class sa section if para sa about us, lagay kalang css para doon
+ basta templated lang ito wag lalagay ng css doon basta seperate css nalang if gusto 
+ mo modify
