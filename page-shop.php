322748296b831af779240847e1764054abc7427b
Christian Santos <it.christiansantos@gmail.com>
2024-06-06 10:51:25 +0800

    Updated files 060624

---
+++ page-shop.php
@@ -0,0 +1,25 @@
+<?php /*Template Name: Shop */
+get_header(); ?>
+
+<section class="index-about l-_hpx">
+    <img src="<?php echo $asset_path; ?>/assets/images/index/about-us.jpg"
+        alt="Innovating Wellness Through Cold Therapy" class="desk_top">
+    <div class="container-fluid">
+        <div class="wrapper">
+            <div class="row">
+                <div class="col-xl-6 col-lg-6 col-12">
+                    <div class="content">
+                        <h2 class="header_text">Lorem ipsum dolor sit amet </h2>
+                        <img src="<?php echo $asset_path; ?>/assets/images/index/about-us.jpg"
+                            alt="Innovating Wellness Through Cold Therapy" class="w-100 mobile">
+                        <p>Lorem ipsum dolor sit amet consectetur. Cras mi enim a fermentum tellus et tristique id. Quam
+                            vulputate tellus sed sapien scelerisque mauris orci at. Suspendisse amet dui senectus est.
+                            Diam sapien sed adipiscing velit orci. Non venenatis dolor et tempus enim.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
