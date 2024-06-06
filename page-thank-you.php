56999bbffd492aa0abb445553a96ebaa2e6346f9
Rima Yumol <remalyn.yumol@gmail.com>
2024-06-06 11:39:34 +0800

    rima-development-thank-you

---
+++ page-thank-you.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+<section class="thank_you_page l-_hpx">
+    <div class="container-fluid">
+        <div class="wrapper">
+            <div class="header_content text-center">
+                <h1 class="header_text">Thank You</h1>
+                <p>Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac
+                    aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                    inceptos himenaeos.</p>
+                <a href="<?php echo $redirect_url; ?>/" class="cta_global bnc">Back to Homepage</a>
+            </div>
+        </div>
+    </div>
+</section>
+<?php get_footer(); ?>
