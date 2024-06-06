2ba3f13953bddd1c50c9e7efa6e9a77b09376126
Rima Yumol <remalyn.yumol@gmail.com>
2024-06-06 13:17:58 +0800

    rima-development-thank-you-404

--- 404.php
+++ 404.php
@@ -1,11 +1,12 @@
-<?php /*Template Name: Thank You */
+<?php /*Template Name: 404 */
 get_header(); ?>
 <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/inc/css/redirection.css">
+
 <section class="redirection l-_hpx">
     <div class="container-fluid">
         <div class="wrapper">
             <div class="_content">
-                <h1 class="header_text">Thank You</h1>
+                <h1 class="header_text">404 error</h1>
                 <p>Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac
                     aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
                     inceptos himenaeos.</p>
@@ -14,4 +15,5 @@ get_header(); ?>
         </div>
     </div>
 </section>
+
 <?php get_footer(); ?>
