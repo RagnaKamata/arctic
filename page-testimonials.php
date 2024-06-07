3a79a0458eb03fa14b8188c3b76493bbbf8f6708
Rima Yumol <remalyn.yumol@gmail.com>
2024-06-07 11:48:17 +0800

    rima-development-testimonials

---
+++ page-testimonials.php
@@ -0,0 +1,188 @@
+<?php /*Template Name: Testimonial Page */
+get_header(); ?>
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/inc/css/column-side.css">
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/inc/css/testimonials.css">
+<section class="cls l-_hpx">
+    <img src="<?php echo $asset_path; ?>/assets/images/testimonials/banner-image.jpg" alt="Banner Image"
+        class="desk_top">
+    <div class="container-fluid">
+        <div class="wrapper">
+            <div class="row">
+                <div class="col-xl-6 col-lg-6 col-12">
+                    <div class="content">
+                        <h1 class="header_text">Lorem ipsum dolor sit amet</h1>
+                        <img src="<?php echo $asset_path; ?>/assets/images/benefits/banner-image.jpg" alt="Banner Image"
+                            class="w-100 mobile">
+                        <p class="m-0">Lorem ipsum dolor sit amet consectetur. Cras mi enim a fermentum tellus et
+                            tristique id. Quam vulputate tellus sed sapien scelerisque mauris orci at. Suspendisse amet
+                            dui senectus est. Diam sapien sed adipiscing velit orci. Non venenatis dolor et tempus enim.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="testimonials-v l-_hpx">
+    <div class="container-fluid">
+        <div class="wrapper">
+            <div class="row">
+                <div class="col-12">
+                    <div class="d-block d-md-flex header_content justify-content-between align-items-center">
+                        <h2 class="header_text m-0 ">See What Our Customers Have to Say</h2>
+                        <a href="http://" target="_blank" class="fw-bold h-4 text-decoration-none d-lg-block d-none">See
+                            all
+                            testimonials <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25"
+                                viewBox="0 0 25 25" fill="none">
+                                <g clip-path="url(#clip0_813_727)">
+                                    <path
+                                        d="M24.7136 11.8087C24.7133 11.8084 24.713 11.8081 24.7127 11.8078L19.61 6.72968C19.2277 6.34926 18.6094 6.35068 18.2289 6.733C17.8484 7.11528 17.8499 7.73359 18.2321 8.11406L21.6581 11.5234H0.976562C0.437207 11.5234 0 11.9606 0 12.5C0 13.0394 0.437207 13.4766 0.976562 13.4766H21.6581L18.2322 16.8859C17.8499 17.2664 17.8484 17.8847 18.2289 18.267C18.6094 18.6494 19.2278 18.6507 19.61 18.2703L24.7127 13.1922C24.713 13.1919 24.7133 13.1915 24.7136 13.1913C25.0961 12.8095 25.0949 12.1892 24.7136 11.8087Z"
+                                        fill="#20201F" />
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_813_727">
+                                        <rect width="25" height="25" fill="white" />
+                                    </clipPath>
+                                </defs>
+                            </svg> </a>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-lg-3 col-md-6 col-12">
+                    <div class="_card d-flex align-items-end text-white position-relative" data-bs-toggle="modal"
+                        data-bs-target="#videoModal"
+                        data-video-src="https://player.vimeo.com/video/947243801?h=cc8f13b0d3&badge=0&autopause=0&player_id=0&app_id=58479">
+                        <div class="svg">
+                            <img src="<?php echo $asset_path; ?>/assets/images/index/zondicons_play-outline.svg"
+                                class="position-absolute z-2 top-0 bottom-0 start-0 end-0 m-auto" alt="">
+                        </div>
+                        <div class="image w-100">
+                            <img src="<?php echo $asset_path; ?>/assets/images/index/Zhemin Sanico.jpg"
+                                class="object-fit-cover w-100 h-100" alt="Zhemin Sanico">
+                        </div>
+                        <div class="content position-absolute z-2 w-100">
+                            <p class="date">April 26,2024</p>
+                            <p class="h-tag fw-bold text-uppercase h-4 m-0">Zhemin Sanico</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-lg-3 col-md-6 col-12">
+                    <div class="_card d-flex align-items-end text-white position-relative" data-bs-toggle="modal"
+                        data-bs-target="#videoModal"
+                        data-video-src="https://player.vimeo.com/video/947256259?h=c5f29bd327&badge=0&autopause=0&player_id=0&app_id=58479">
+                        <div class="svg"> <img
+                                src="<?php echo $asset_path; ?>/assets/images/index/zondicons_play-outline.svg"
+                                class=" position-absolute z-2  top-0 bottom-0 start-0 end-0 m-auto" alt=""> </div>
+                        <div class="image w-100"> <img
+                                src="<?php echo $asset_path; ?>/assets/images/index/Peter Navarro.jpg"
+                                class="object-fit-cover w-100 h-100" alt="Peter Navarro"> </div>
+                        <div class="content position-absolute z-2 w-100">
+                            <p class="date">April 26,2024</p>
+                            <p class="h-tag fw-bold text-uppercase h-4 m-0">Peter Navarro</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-lg-3 col-md-6 col-12">
+                    <div class="_card d-flex align-items-end text-white position-relative" data-bs-toggle="modal"
+                        data-bs-target="#videoModal"
+                        data-video-src="https://player.vimeo.com/video/947255438?h=a37661d36a&badge=0&autopause=0&player_id=0&app_id=58479">
+                        <div class="svg"> <img
+                                src="<?php echo $asset_path; ?>/assets/images/index/zondicons_play-outline.svg"
+                                class=" position-absolute z-2  top-0 bottom-0 start-0 end-0 m-auto" alt=""> </div>
+                        <div class="image w-100"> <img
+                                src="<?php echo $asset_path; ?>/assets/images/index/Kevin George Alcantara.jpg"
+                                class="object-fit-cover w-100 h-100" alt="Kevin George Alcantara"> </div>
+                        <div class="content position-absolute z-2 w-100">
+                            <p class="date">April 26,2024</p>
+                            <p class="h-tag fw-bold text-uppercase h-4 m-0">Kevin George Alcantara</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-lg-3 col-md-6 col-12">
+                    <div class="_card d-flex align-items-end text-white position-relative" data-bs-toggle="modal"
+                        data-bs-target="#videoModal"
+                        data-video-src="https://player.vimeo.com/video/947242307?h=6c63e57089&badge=0&autopause=0&player_id=0&app_id=58479">
+                        <div class="svg"> <img
+                                src="<?php echo $asset_path; ?>/assets/images/index/zondicons_play-outline.svg"
+                                class=" position-absolute z-2  top-0 bottom-0 start-0 end-0 m-auto" alt=""> </div>
+                        <div class="image w-100"> <img
+                                src="<?php echo $asset_path; ?>/assets/images/index/Jamell Paulo Buan.jpg"
+                                class="object-fit-cover w-100 h-100" alt="Jamell Paulo Buan"> </div>
+                        <div class="content position-absolute z-2 w-100">
+                            <p class="date">April 26,2024</p>
+                            <p class="h-tag fw-bold text-uppercase h-4 m-0">Jamell Paulo Buan</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-12 col-lg-6 col-md-6 col-12 d-lg-none d-sm-block">
+                    <div class="d-flex h-100 header_content ">
+                        <a href="http://" target="_blank" class="fw-bold h-4 text-decoration-none">See all testimonials
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25"
+                                fill="none">
+                                <g clip-path="url(#clip0_813_727)">
+                                    <path
+                                        d="M24.7136 11.8087C24.7133 11.8084 24.713 11.8081 24.7127 11.8078L19.61 6.72968C19.2277 6.34926 18.6094 6.35068 18.2289 6.733C17.8484 7.11528 17.8499 7.73359 18.2321 8.11406L21.6581 11.5234H0.976562C0.437207 11.5234 0 11.9606 0 12.5C0 13.0394 0.437207 13.4766 0.976562 13.4766H21.6581L18.2322 16.8859C17.8499 17.2664 17.8484 17.8847 18.2289 18.267C18.6094 18.6494 19.2278 18.6507 19.61 18.2703L24.7127 13.1922C24.713 13.1919 24.7133 13.1915 24.7136 13.1913C25.0961 12.8095 25.0949 12.1892 24.7136 11.8087Z"
+                                        fill="#20201F" />
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_813_727">
+                                        <rect width="25" height="25" fill="white" />
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+                <!-- Modal -->
+                <div class="modal fade" id="videoModal" tabindex="-1" aria-labelledby="videoModalLabel"
+                    aria-hidden="true">
+                    <div class="modal-dialog modal-lg modal-dialog-centered">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="videoModalLabel">Testimonial Video</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body position-relative"
+                                style="padding:0; height:0; padding-top:56.25%; overflow:hidden;">
+                                <iframe id="videoIframe" src="" frameborder="0"
+                                    allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
+                                    style="position:absolute;top:0;left:0;width:100%;height:100%;"
+                                    title="Video"></iframe>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+<?php get_footer(); ?>
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        var videoModal = document.getElementById('videoModal');
+        videoModal.addEventListener('show.bs.modal', function (event) {
+            var button = event.relatedTarget; // Button that triggered the modal
+            var videoSrc = button.getAttribute('data-video-src'); // Extract info from data-* attributes
+            var iframe = videoModal.querySelector('#videoIframe');
+            iframe.src = videoSrc;
+        });
+
+        videoModal.addEventListener('hidden.bs.modal', function () {
+            var iframe = videoModal.querySelector('#videoIframe');
+            iframe.src = ''; // Stop the video when the modal is closed
+        });
+    });
+</script>
+
+<script>
+    $("section.youtube .youtube-iframe img").click(function () {
+        $(this).replaceWith(`<div class="video"><iframe src="https://player.vimeo.com/video/947241434?h=eee802d6e7&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write"  title="Arctic Product Preview - Horizontal"></iframe></div>`)
+    })
+</script>
+<script src="https://player.vimeo.com/api/player.js"></script>
